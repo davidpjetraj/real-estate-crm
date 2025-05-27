124f5f78c3713ad19a110928335d3378e690667f
dednikollbibaj <dedi@tetbit.com>
2025-05-27 09:36:52 +0200

    update env

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8080/graphql', 
+  uri: 'http://192.168.1.82:8080/graphql', 
 });
 const authLink = setContext((_, { headers }) => {
   const token = typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
